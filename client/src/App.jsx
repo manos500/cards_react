556de5c9f9487c6bad9e84c321882e19d0e06b5d
Manos <manosnikitakis3@gmail.com>
2025-07-27 12:15:14 +0300

    Added filterSort context provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import './App.css'
 import { OpenPack } from './components/OpenPack';
 import { PackInfo } from './components/PackInfo';
 import {PackProvider} from  './Contexts/PackContext';
+import { FilterSortProvider } from './Contexts/FilterSortContext';
 
 
 function App() {
@@ -18,16 +19,18 @@ function App() {
     
     <BrowserRouter>
       <Navbar/>
-      <PackProvider>
-        <Routes>
-          <Route path="/" element = {<Home/>}/>
-          <Route path="/shop" element = {<Shop/>}/>
-          <Route path="/shop/buy" element={<OpenPack />} />
-          <Route path="/shop/info" element={<PackInfo />} />
-          <Route path="/collection" element = {<Collection/>}/>
-          <Route path="/collection/filter" element = {<Filter/>}/>
-        </Routes>
-      </PackProvider>
+      <FilterSortProvider>  
+        <PackProvider>
+          <Routes>
+            <Route path="/" element = {<Home/>}/>
+            <Route path="/shop" element = {<Shop/>}/>
+            <Route path="/shop/buy" element={<OpenPack />} />
+            <Route path="/shop/info" element={<PackInfo />} />
+            <Route path="/collection" element = {<Collection/>}/>
+            <Route path="/collection/filter" element = {<Filter/>}/>
+          </Routes>
+        </PackProvider>
+      </FilterSortProvider> 
       <Footer/>
     </BrowserRouter>
     
