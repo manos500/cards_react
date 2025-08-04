8038d695a22e5b4b3a99a5d7295163d0a2ccf1b0
Manos <manosnikitakis3@gmail.com>
2025-08-04 18:04:02 +0300

    added database connection

---
+++ server/connection.js
@@ -0,0 +1,15 @@
+import pg from "pg";
+import dotenv from 'dotenv';
+
+const {Client} = pg;
+dotenv.config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+export {client}
