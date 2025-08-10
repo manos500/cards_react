521b47cf929545ca2c38eb2a7592a4183dd95966
Manos <manosnikitakis3@gmail.com>
2025-08-10 18:29:00 +0300

    added Error component

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,10 @@
+import '../styles/error.css'
+
+export const Error = () => {
+  return (
+    <>
+     <div className="error_icon">⚠️</div>
+     <p className="error_message">Oops! Failed to load</p>
+    </>
+  )
+}
