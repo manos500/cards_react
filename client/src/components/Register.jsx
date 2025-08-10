1e2698a291b7f10dabff4eb3470520002968ccf9
Manos <manosnikitakis3@gmail.com>
2025-08-10 18:34:11 +0300

    added Register component

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { registerUser } from '../api/pack1';
+
+export const Register = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try{
+      const data = await registerUser(username, email, password)
+      console.log("Login successful", data);
+      navigate("/login");
+    } catch (error){
+      alert(error.message);
+
+    // καθάρισμα input πεδίων σε αποτυχημένο login
+      setUsername('');
+      setEmail('')
+      setPassword('');
+    }
+  }
+
+  return (
+    <div className='loginRegister_bg light'>
+      <div className="loginRegister_container" id="myForm">
+        <h1>Register</h1>
+        <form className="form_container" id="form" onSubmit={handleRegister}>
+          <div className="txt_field">
+            <input type="text" id="username" value={username} required onChange={(e) => {setUsername(e.target.value)}}/>
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div className="txt_field">
+                <input type="text" id="email" required value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+                <span></span>
+                <label>Email</label>
+            </div>
+          <div className="txt_field">
+            <input type="password" id="password" required value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+            <span></span>
+            <label>Password</label>
+          </div>
+          <button className="loginRegister_btn" type='submit'>Register</button>
+          <div className="loginRegister_link">
+            Already registered? 
+            <Link to="/register">
+              <span>Login</span>
+            </Link>
+          </div>
+        </form>
+    </div>
+  </div>
+  )
+}
