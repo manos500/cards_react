ddf69441cdd6cbf276b824ece1157ccda11a94e6
Manos <manosnikitakis3@gmail.com>
2025-09-16 10:26:35 +0300

    Updated api for fetching cards

--- client/src/api/pack1.js
+++ client/src/api/pack1.js
@@ -43,10 +43,13 @@ export const unlockCards = async (userId, cardId) => {
 
 
 export const fetchUserCollection = async (userId) => {
-  const res = await fetch(`http://localhost:5000/collection/${userId}`);
-  const data = await res.json();
-  console.log("UserCollection from API:", data);
-  return data;
+  try {
+    const response = await axios.get(`http://localhost:5000/collection/${userId}`);
+    console.log("UserCollection from API:", response.data);
+    return response.data;
+  }catch (error) {
+    console.error('fetchUserCollection API error:', error.response?.data || error.message);
+  }
 };
 
 
@@ -57,19 +60,18 @@ export const fetchCards = async () => {
       return response.data.map((card) => ({
         id: card.id,
         name: card.name,
+        cardType: card.type,
+        SpellTrapCardType: card.humanReadableCardType,
+        filterCardTypes: card.frametype,
+        description: card.description,
+        monsterType: card.race,
         attack: card.attack,
         defense: card.defense,
-        filterCardType: card.frametype,
-        image: card.card_images[0].image_url_small,
-        cardType: card.type,
-        attribute: card.attribute,
-        level: card.level,
-        monsterType: card.race,
-        SpellTrapCardType: card.humanReadableCardType,
-        rarity: card.card_sets ? card.card_sets[1].set_rarity : "Unknown",
-        description: card.description,
-        cardPack: card.card_sets[0].set_name,
-        unlocked: false,
+        level: card.level,    
+        attribute: card.attribute,   
+        rarity: card.rarity,
+        cardPack: card.set,
+        image: card.image,
       }));
 }
 
